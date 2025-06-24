2a2b887a479243e852714f14496246858df4f1ad
Elise Horne <Elise@MacBook-Air.home.local>
2025-06-24 15:20:55 -0500

    family order

--- Day3.py
+++ Day3.py
@@ -5,15 +5,15 @@
 
 # TODO: Complete the following tasks.
 #  * Save the name Matthew as a variable called brother.
-#  * Save the name Renae as a variable called mother.
+#  * Save the name Liz as a variable called mother.
 #  * Save the name Kia as a variable called best_sister_ever.
-#  * Save the name Meagan as a variable called okayest_sister.
-#  * Save the name Aleidy as a variable called girlfriend.
+#  * Save the name Kayla as a variable called okayest_sister.
+#  * Save the name Penny as a variable called dog.
 #  * Create an empty list called family_members.
 #  * Create an empty list called family_rankings.
 #  * Add your family members (the variables you created) to the list family_members using the `.append` method.
 #  * Rank those family members on a scale of 1-5 by using the `.append` method to add a ranking to the list
-#  family_rankings. Here is how they should be ranked: Renae -> 1, Meagan -> 2, Matthew -> 3, Aleidy -> 4, Kia -> 5.
+#  family_rankings. Here is how they should be ranked: Liz -> 1, Kayla -> 2, Matthew -> 3, Penny -> 4, Kia -> 5.
 #  For example, if Matthew is first in you family_members list, then you would need to append his ranking FIRST bu using
 #  family_rankings.append(3).
 
@@ -27,3 +27,37 @@
 #  * "Who is your other sister, the just-okay one?" should appear in the terminal.
 #  CHALLENGE: The answer (Meagan of course) should appear in the terminal. You MUST refer to Megan using her index in
 #  the family_members list. For example: family_members[1].
+
+brother = "Matthew"
+mother = "Liz"
+best_sister_ever = "Kia"
+okayest_sister_ever = "Kayla"
+dog = "Penny"
+
+family_members = []
+
+family_members.append(brother)
+family_members.append(mother)
+family_members.append(best_sister_ever)
+family_members.append(okayest_sister_ever)
+family_members.append(dog)
+
+
+family_rankings = []
+
+family_rankings.append(3)
+family_rankings.append(1)
+family_rankings.append(5)
+family_rankings.append(2)
+family_rankings.append(4)
+
+print("Who are your top five favorite family members?")
+print(family_members)
+
+print("How would you rank those family members on a scale of 1-5, 5 being the most awesome?")
+print(family_rankings)
+
+print("Wow! I need to meet this Kia person, she must be awesome!!!")
+
+print("Who is your other sister, the just-okay one?")
+print(family_members[3])
--- Day3Notes.md
+++ Day3Notes.md
@@ -8,25 +8,25 @@
 ## Question 1: What are lists in Python? How do you create an empty list?
 Type your answer in the space below.
 ```
-
+They are similar to arrays, but they can store any kind of variable. 
 ```
 
 ## Question 2: How do you add something to a list in Python?
 Type your answer in the space between the back-ticks (`). 
 ```
-
+You use the .append method. 
 ```
 
 ## Question 3: What is an index of a list?
 Type your answer in the space between the back-ticks (`).
 ```
-
+It refers to the position of an element within the list. Python is zero-based, so the first element is at index 0
 ```
 
 ## Question 4: For the following list, what name is at index 2? What name is at index 4? my_list = ["Justin", "Aleidy", "Kia", "Matt", "Renae"]
 Type your answer in the space between the back-ticks (`).
 ```
-
+Kia is at index 2 and Renae is at index 4. 
 ```
 
 # Go to Day3.py for further instructions!
